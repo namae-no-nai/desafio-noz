a907adacc12b8421892bc18992988ec2a9ebde46
guilherme luiz <guilhermebluiz@hotmail.com>
2022-08-10 21:08:33 -0300

    add seeds to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Author.count.zero?
+  10.times do
+    author = Author.create(
+      name: Faker::Book.author,
+      age: rand(20..99),
+      main_genre: Faker::Book.genre
+    )
+
+    3.times do
+      author.books.create(
+        title: Faker::Book.title,
+        description: Faker::Lorem.sentence(word_count: 10),
+        genre: Faker::Book.genre,
+        publisher: Faker::Book.publisher,
+        published_at: Date.today - rand(1000..3000).days
+      )
+    end
+  end
+end
